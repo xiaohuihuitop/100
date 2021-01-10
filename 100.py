04355c287ab51b3e293a6465b52df015e3fcf82e
xhh <tangchaohuitop@outlook.com>
2021-01-10 23:55:51 +0800

    扫描指定目录下 所有文件 包括子目录

--- 100.py
+++ 100.py
@@ -1,33 +1,45 @@
-# 3*4  是3个列表 每个列表4个元素
-Test_li = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+import os
 
 """
-展平
+for root, dirs, files in os.walk(file_dir):
+        # print(root)  # 当前目录路径
+        # print(dirs)  # 当前路径下所有子目录
+        # print(files)  # 当前路径下所有非目录子文件
+一共有多少级目录 就会循环多少次
+root 会逐渐深入目录  然后加上 files 中的文件名 就可以得到所有的文件了
 """
 
-li = [i for row in Test_li for i in row]
-print(li)
 
-li = []
-for row in Test_li:
-    for i in row:
-        li.append(i)
+def File_Scan(file_dir):
+    return_list = []
+    for root, dirs, files in os.walk(file_dir):  # 得到当前目录 root 目录列表 dirs 文件列表 files
+        # print(root)
+        # print(dirs)
+        # print(files)
+        for file in files:
+            file_name = root + "\\" + file
+            if os.path.splitext(file_name)[-1] == ".py":
+                return_list.append(file_name)
 
-print(li)
+    return return_list
 
 
-"""
-改成 4*3
-"""
+def File_Scan2(open_file_path):
+    root_dir = open_file_path
+    return_list = []
+    file_list = os.listdir(root_dir)  # 列出文件夹下所有的目录与文件
+    for i in range(0, len(file_list)):
+        com_path = os.path.join(root_dir, file_list[i])
+        if os.path.isfile(com_path):
+            return_list.append(com_path)
+        if os.path.isdir(com_path):
+            return_list.extend(File_Scan2(com_path))
+    return return_list
 
-li = [[row[i] for row in Test_li] for i in range(4)]
-print(li)
 
-li = []
-for i in range(4):
-    temp_li = []
-    for row in Test_li:
-        temp_li.append(row[i])
-    li.append(temp_li)
+File_List = File_Scan(os.getcwd())
+# print(File_List)
 
-print(li)
+
+for fil_name in File_List:
+    print(fil_name)
